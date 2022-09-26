f310a4323197d5a950887ed685e4ac5b62279120
Robert Grzyb <robertgrzyb96@gmail.com>
2022-09-26 21:02:12 +0200

    bux fixes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-let songoSSJ = document.querySelector(".songoSSJ");
+let songoSSJ = document.querySelector(".js-songoSSJ");
 let songoDuzy = document.querySelector(".songoDuzy");
 let songoMaly = document.querySelector(".songoMaly");
-let button = document.querySelector(".button");
+let button = document.querySelector(".js-button");
 let photoButtonElement = document.querySelector(".photoButtonElement");
-let heightElement = document.querySelector(".js-height");
 
 function double(number) {
   return number * 2;
@@ -13,13 +12,9 @@ console.log(double(5));
 console.log(double(40));
 console.log(double(40));
 
-heightElement.addEventListener("input", () => {
-  console.log(`Aktualny wzrost i waga małego człowieczka: ${heightElement.value}`);
-});
-
 button.addEventListener("click", () => {
-  songoSSJ.remove();
-
+  songoSSJ.classList.toggle("hidden");
+  button.innerText = songoSSJ.classList.contains("hidden") ? "Pokaż zdjęcie" : "Ukryj zdjęcie";
 });
 
 photoButtonElement.addEventListener("click", () => {
